fec3b3c7cdb787949149cee1be8ae28c2756fea7
BeatrizFrazao <ana@gmail.com>
2026-04-17 15:04:37 -0300

    primeiro commit

---
+++ exc1.js
@@ -0,0 +1,8 @@
+//Escreva um programa que leia dois números inteiros fornecidos pelo usuário e exiba a soma deles.
+let p1 = Number(prompt("digite um número"))
+let p2 = Number(prompt("digite outro número"))
+const resultado = (p1 + p2) 
+//
+document.write ("<br>seu primeiro número é; " + p1)
+document.write ("<br>Seu segundo número é: " + p2)
+document.write("<br>Seu resultado é: " + resultado)
